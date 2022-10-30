049da920ebe55c5bcb8b00058610d7e515e5a18a
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-10-30 10:13:46 +0000

    Added row component with api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Genre from './Genre';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Header />
       <Genre />
     </div>
