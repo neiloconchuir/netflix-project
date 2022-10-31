8fa33854bcc0979737a75d0b252808ae54eb03f7
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-10-31 17:06:13 +0000

    Nav bar added

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Netflix project build by Neil O'Conchuir, and <a  >open-source on GitHub</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './nav.css';
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/1/15/Logonfx.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav
